f15061a7afa328ce01d4986aa73ffa85f99fdcfc
Parker Mortensen <parker.b.mortensen@gmail.com>
2023-03-04 21:26:37 -0700

    attempt #1 to implement oauth2 on Render

--- oauth.js
+++ oauth.js
@@ -3,12 +3,12 @@ const OAuth2Strategy = require('passport-oauth2').Strategy;
 
 // Step 2: create OAuth2 client configuration object
 const config = {
-  clientID: 'your-client-id',
-  clientSecret: 'your-client-secret',
-  callbackURL: 'http://localhost:8080/auth/callback',
-  authorizationURL: 'https://example.com/oauth2/authorize',
-  tokenURL: 'https://example.com/oauth2/token'
-};
+    clientID: 'your-client-id',
+    clientSecret: 'your-client-secret',
+    callbackURL: 'https://cse341finalproject.onrender.com/auth/callback',
+    authorizationURL: 'https://cse341finalproject.onrender.com/authorize',
+    tokenURL: 'https://cse341finalproject.onrender.com/token'
+  };  
 
 // Step 3: configure Passport.js to use OAuth2 strategy
 passport.use(new OAuth2Strategy(config, (accessToken, refreshToken, profile, cb) => {
@@ -16,7 +16,17 @@ passport.use(new OAuth2Strategy(config, (accessToken, refreshToken, profile, cb)
 }));
 
 // define middleware function to authenticate requests
-const authenticate = passport.authenticate('oauth2', { session: false });
+const authenticate = (req, res, next) => {
+  passport.authenticate('oauth2', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(401).json({
+        message: 'Unauthorized'
+      });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
 
 // export middleware function
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('./db.js');
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2').Strategy;
 
 const app = express();
 const port = 8080;
@@ -7,15 +9,33 @@ const port = 8080;
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-// import OAuth middleware
-const oauth = require('./oauth')(app);
+// Step 2: create OAuth2 client configuration object
+const config = {
+    clientID: 'your-client-id',
+    clientSecret: 'your-client-secret',
+    callbackURL: 'https://cse341finalproject.onrender.com/auth/callback',
+    authorizationURL: 'https://cse341finalproject.onrender.com/authorize',
+    tokenURL: 'https://cse341finalproject.onrender.com/token'
+  };  
+
+// Step 3: configure Passport.js to use OAuth2 strategy
+passport.use(new OAuth2Strategy(config, (accessToken, refreshToken, profile, cb) => {
+  // handle user authentication and authorization
+}));
+
+// define middleware function to authenticate requests
+const authenticate = passport.authenticate('oauth2', { session: false });
+
+// use passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(express.json())
   // protect routes using OAuth middleware
-  .use('/account', oauth.authenticate, require('./routes/account'))
-  .use('/transaction', oauth.authenticate, require('./routes/transaction'));
+  .use('/account', authenticate, require('./routes/account'))
+  .use('/transaction', authenticate, require('./routes/transaction'));
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
